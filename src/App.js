5a07d260da24b229ce76720fd31ea72545df2c61
Yaerin Park <programmer94@naver.com>
2023-09-06 00:40:08 +0900

    TODO리스트

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
